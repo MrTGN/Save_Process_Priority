f07a980051ab846b58af4a78d4e450ca4d4213df
MrTGN <spbtgn@gmail.com>
2016-09-29 11:44:07 +0300

    Rewrote save_process_priority.cmd

--- save_process_priority.cmd
+++ save_process_priority.cmd
@@ -1,131 +1,122 @@
-::Copyright c 2011 Tsiryuta G. N. <spbtgn@gmail.com>
+:: Copyright Tsiryuta G. N. <spbtgn@gmail.com>
 @echo off
+title Copyright ^c 2016 Tsiryuta G. N. ^<spbtgn^@gmail.com^>
 cls
-	:start_prog
-echo            Copyright ^c 2011 Tsiryuta G. N. ^<spbtgn^@gmail.com^>
-echo.
-set question1=
-set question2=
-set question3=
-echo ###########################################################################
-echo #                                                                         #
-echo #     С помощью этого пакетного файла вы можете назначить приоритет       #
-echo #  любому процессу, с которым он будет всегда автоматически запускаться,  #
-echo #          либо же удалить назначенный вами ранее приоритет.              #
-echo #                                                                         # 
-echo ###########################################################################
 @echo.
-	:qstn1
-set question1=
-set question2=
-set question3=
+echo #########################################################################
+echo #                                                                       #
+echo #      С помощью этого пакетного файла вы можете назначить приоритет    #
+echo # любому процессу, с которым он будет всегда автоматически запускаться, #
+echo #             либо же удалить назначенный ранее приоритет.              #
+echo #                                                                       # 
+echo #########################################################################
+
+:ask_what_to_do
 @echo.
 echo Выберите желаемую операцию:
-echo 1 - Назначить приоритет определенному процессу;
-echo 2 - Удалить запись о назначенном ранее приоритете определенному процессу;
-echo 3 - Выйти.
+echo A - Назначить приоритет процессу;
+echo D - Удалить назначеный ранее приоритет процесса;
+echo E - Выйти.
 @echo.
-set /P question1="Введите одну из цифр, соответствующую желаемой операции (Пример: 2): ":
+choice /C EDA /N /M "A, D или E? "
+if errorlevel 3	goto add_record
+if errorlevel 2	goto delete_record
+if errorlevel 1	goto end_script
+echo Ответ не верный. Попробуйте еще раз.
+goto ask_what_to_do
+
+:add_record
+cls
+echo #########################################################################
+echo #                                                                       #
+echo #                     Назначение приоритета процессу                    #
+echo #                                                                       #
+echo #########################################################################
 @echo.
 @echo.
-if %question1%==1 (
-	goto create_record
-} else (
-	goto q1_ans2
-)
-	:q1_ans2
-if %question1%==2 (
-	goto delete_record
-) else (
-	goto q1_ans3
-)
-	:q1_ans3
-if %question1%==3 (
-	goto end_prog
-) else (
-	echo Ответ не верный. Попробуйте еще раз.
-	goto qstn1
-)
-	:create_record
+set process_name=
+set /P process_name="Введите название процесса (Пример: example.exe): ":
 @echo.
-set question1=
-set question2=
-set /P question2="Введите название процесса (Пример: example.exe): ":
+echo Выберите приоритет для процесса:
+echo H - High (высокий);
+echo A - AboveNormal (выше среднего);
+echo N - Normal (средний);
+echo B - BelowNormal (ниже среднего);
+echo I - Idle (низкий).
+
+:set_process_priority
 @echo.
-echo ##################################################
-echo #                                                #
-echo #   Возможные приоритеты для процессов:          #
-echo #   1 - приоритет Idle (низкий);                 #
-echo #   2 - приоритет BelowNormal (ниже среднего);   #
-echo #   3 - приоритет Normal (средний);              #
-echo #   4 - приоритет AboveNormal (выше среднего);   #
-echo #   5 - приоритет High (высокий).                #
-echo #                                                #
-echo ##################################################
+set process_priority=
+choice /C IBNAH /N /M "H, A, N, B или I? "
+:: High
+if errorlevel 5 (
+	set "process_priority=0x00000003"
+	goto reg_add_command
+)
+:: AboveNormal
+if errorlevel 4 (
+	set "process_priority=0x00000006"
+	goto reg_add_command
+)
+:: Normal
+if errorlevel 3 (
+	set "process_priority=0x00000002"
+	goto reg_add_command
+)
+:: BelowNormal
+if errorlevel 2 (
+	set "process_priority=0x00000005"
+	goto reg_add_command
+)
+:: Idle
+if errorlevel 1 (
+	set "process_priority=0x00000001"
+	goto reg_add_command
+)	
+echo Ответ не верный. Попробуйте еще раз.
+goto set_process_priority
+:reg_add_command
 @echo.
-	:qstn3
-set question3=
-set /P question3="Введите одну из цифр, соответствующую определенному приоритету (Пример: 1): ":
-if %question3%==1 (
-	reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\%question2%\PerfOptions" /v CpuPriorityClass /t REG_DWORD /d 0x00000001
-	goto continue_qstn
-) else (
-	goto q3_ans2
-)
-	:q3_ans2
-if %question3%==2 (
-	reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\%question2%\PerfOptions" /v CpuPriorityClass /t REG_DWORD /d 0x00000005
-	goto continue_qstn
-) else (
-	goto q3_ans3
-)
-	:q3_ans3
-if %question3%==3 (
-	reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\%question2%\PerfOptions" /v CpuPriorityClass /t REG_DWORD /d 0x00000008
-	goto continue_qstn
-) else (
-	goto q3_ans4
-)
-	:q3_ans4
-if %question3%==4 (
-	reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\%question2%\PerfOptions" /v CpuPriorityClass /t REG_DWORD /d 0x00000006
-	goto continue_qstn
-) else (
-	goto q3_ans5
-)
-	:q3_ans5
-if %question3%==5 (
-	reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\%question2%\PerfOptions" /v CpuPriorityClass /t REG_DWORD /d 0x00000003
-	goto continue_qstn
-) else (
-	echo Вы ввели неправильное значение. Выберите значение из списка выше и повторите ввод.
-	goto qstn3
-)
-set question2=
-set question3=
-goto continue_qstn
-	:delete_record
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\%process_name%\PerfOptions" /v CpuPriorityClass /t REG_DWORD /d %process_priority%
+set process_name=
+set process_priority=
+goto ask_about_continue_working
+
+:delete_record
+cls
+echo #########################################################################
+echo #                                                                       #
+echo #            Удаление назначенного ранее приоритета процесса            #
+echo #                                                                       #
+echo #########################################################################
 @echo.
-set question1=
-set question2=
-set /P question2="Введите название процесса (Пример: example.exe): ":
-reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\%question2%"
-set question2=
-	:continue_qstn
 @echo.
-set /P continue_question="Хотите продолжить работать в пакетном файле? (y - Да; n - Нет): ":
+echo Выполнить поиск разделов реестра для процессов с параметром CpuPriorityClass?
+choice /C NY /N /T 10 /D Y /M "(Y - Да, N - Нет.) "
 @echo.
-if "%continue_question%"=="y" (
-	goto qstn1
-} else (
-	goto c_q_ans_n
+if errorlevel 2 (
+	reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options" /s /v CpuPriorityClass
+	@echo.
+	choice /C YN /N /M "Продолжить процедуру удаления (Y - Да, N - Нет.) "
+	if errorlevel 2 goto ask_about_continue_working
 )
-	:c_q_ans_n
-if "%continue_question%"=="n" (
-	goto end_prog
-} else (
-	echo Ответ не верный. Попробуйте еще раз.
-	goto continue_qstn
+@echo.
+set process_name=
+set /P process_name="Введите название процесса (Пример: example.exe): ":
+@echo.
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\%process_name%"
+set process_name=
+
+:ask_about_continue_working
+@echo.
+choice /C NY /N /M "Продолжить работу? (Y - Да, N - Нет.) "
+::Yes
+if errorlevel 2	(
+	cls
+	goto ask_what_to_do
 )
-	:end_prog
+::No
+if errorlevel 1 goto end_script
+
+:end_script
 exit
